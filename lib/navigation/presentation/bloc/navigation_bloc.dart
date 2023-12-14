8ce4126aafff0af4a5813388f30b7b857dc9504f
Hadiya <hadiyaaamir222@gmail.com>
2023-12-14 19:00:32 +0500

    navigation bloc

--- lib/navigation/presentation/bloc/navigation_bloc.dart
+++ lib/navigation/presentation/bloc/navigation_bloc.dart
@@ -1,7 +1,6 @@
-import 'dart:async';
-
-import 'package:bloc/bloc.dart';
+import 'package:chatbot/navigation/navigation.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
 
 export 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -10,13 +9,13 @@ part 'navigation_state.dart';
 
 class NavigationBloc extends Bloc<NavigationEvent, NavigationState> {
   NavigationBloc() : super(const NavigationInitial()) {
-    on<NavigationCustomEvent>(_onNavigationEvent);
+    on<NavigationIndexChanged>(_onNavigationIndexChanged);
   }
 
-  FutureOr<void> _onNavigationEvent(
-    NavigationCustomEvent event,
+  void _onNavigationIndexChanged(
+    NavigationIndexChanged event,
     Emitter<NavigationState> emit,
   ) {
-    // TODO: Add Logic
+    emit(state.copyWith(currentIndex: event.selectedIndex));
   }
 }
--- lib/navigation/presentation/view/navigation_view.dart
+++ lib/navigation/presentation/view/navigation_view.dart
@@ -5,6 +5,6 @@ class NavigationView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const NavigationBody();
+    return const Scaffold();
   }
 }
