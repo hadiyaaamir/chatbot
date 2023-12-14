e7cb95c521eb54a45d0df367703ad9857593be5c
Hadiya <hadiyaaamir222@gmail.com>
2023-12-14 22:41:36 +0500

    nabar pages status maintained

--- lib/navigation/presentation/widgets/widgets.dart
+++ lib/navigation/presentation/widgets/widgets.dart
@@ -1,5 +1,3 @@
-import 'package:chatbot/chatbot/chatbot.dart';
-import 'package:chatbot/navigation/presentation/models/models.dart';
 import 'package:chatbot/navigation/presentation/presentation.dart';
 import 'package:flutter/material.dart';
 
