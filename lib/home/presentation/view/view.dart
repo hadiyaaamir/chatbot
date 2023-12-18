b8412049452b19eb8b57a70cc1e2540620842d66
Hadiya <hadiyaaamir222@gmail.com>
2023-12-18 18:13:48 +0500

    suggestions added to home page

--- lib/home/presentation/view/view.dart
+++ lib/home/presentation/view/view.dart
@@ -1,9 +1,8 @@
+import 'package:chatbot/chatbot/chatbot.dart';
 import 'package:chatbot/events/events.dart';
 import 'package:chatbot/home/home.dart';
-import 'package:chatbot/home/presentation/widgets/events_filter_buttons.dart';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
 part 'home_page.dart';
 part 'home_view.dart';
--- lib/home/presentation/widgets/widgets.dart
+++ lib/home/presentation/widgets/widgets.dart
@@ -1,1 +1,3 @@
 export 'events_list.dart';
+export 'events_filter_buttons.dart';
+export 'suggestion_messages.dart';
