6a8a13a3262e74541b71365e899841c0d2923698
Hadiya <hadiyaaamir222@gmail.com>
2023-12-19 11:18:27 +0500

    ticket clipped semicircles

--- lib/payment/presentation/widgets/widgets.dart
+++ lib/payment/presentation/widgets/widgets.dart
@@ -1,6 +1,1 @@
-import 'package:chatbot/utils/widgets/button.dart';
-import 'package:chatbot/payment/presentation/view/view.dart';
-import 'package:chatbot/tickets/tickets.dart';
-import 'package:flutter/material.dart';
-
 part 'payment_button.dart';
