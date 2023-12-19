cab684f9decd2c08c325a05314873bb7c542ce67
Hadiya <hadiyaaamir222@gmail.com>
2023-12-19 16:56:57 +0500

    payment ui updated

--- lib/payment/presentation/view/view.dart
+++ lib/payment/presentation/view/view.dart
@@ -1,8 +1,6 @@
+import 'package:chatbot/payment/payment.dart';
 import 'package:chatbot/tickets/tickets.dart';
-import 'package:chatbot/utils/widgets/button.dart';
-import 'package:chatbot/utils/widgets/dotted_divider.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 
 part 'payment_page.dart';
 part 'payment_view.dart';
