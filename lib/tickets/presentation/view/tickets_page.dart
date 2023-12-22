6a3107af79c18e10fe904c496a6b2335eba17ef9
Hadiya <hadiyaaamir222@gmail.com>
2023-12-22 12:21:08 +0500

    basic tickets page

---
+++ lib/tickets/presentation/view/tickets_page.dart
@@ -0,0 +1,16 @@
+part of 'view.dart';
+
+class TicketsPage extends StatelessWidget {
+  const TicketsPage({super.key});
+
+  static Route<dynamic> route() {
+    return MaterialPageRoute<dynamic>(
+      builder: (_) => const TicketsPage(),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const TicketsView();
+  }
+}
---
+++ lib/tickets/presentation/view/tickets_view.dart
@@ -0,0 +1,12 @@
+part of 'view.dart';
+
+class TicketsView extends StatelessWidget {
+  const TicketsView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: Text('Tickets')),
+    );
+  }
+}
