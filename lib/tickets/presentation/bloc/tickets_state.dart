91abe645cc59d41fdb394dbf13f5dd454e3fc2ac
Hadiya <hadiyaaamir222@gmail.com>
2023-12-22 12:52:02 +0500

    basic bloc

--- lib/tickets/presentation/bloc/tickets_state.dart
+++ lib/tickets/presentation/bloc/tickets_state.dart
@@ -8,6 +8,13 @@ class TicketsState extends Equatable {
   final List<Ticket> tickets;
   final TicketsStatus status;
 
+  TicketsState copyWith({List<Ticket>? tickets, TicketsStatus? status}) {
+    return TicketsState(
+      tickets: tickets ?? this.tickets,
+      status: status ?? this.status,
+    );
+  }
+
   @override
   List<Object> get props => [tickets, status];
 }
